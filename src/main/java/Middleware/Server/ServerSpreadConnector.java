3948ba8ca4cd79ce4f3dc7e60d584b5a303f91f6
joaop21 <jpsilva9898@gmail.com>
2020-05-03 14:18:38 +0100

    Add multicast method

--- src/main/java/Middleware/Server/ServerSpreadConnector.java
+++ src/main/java/Middleware/Server/ServerSpreadConnector.java
@@ -3,11 +3,16 @@ package Middleware.Server;
 import spread.SpreadConnection;
 import spread.SpreadException;
 import spread.SpreadGroup;
+import spread.SpreadMessage;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.UUID;
 
+/**
+ * Singleton class
+ * Only one instance of this class in runtime
+ * */
 public class ServerSpreadConnector {
     private static SpreadConnection spreadConn = null;
     private static String connName = null;
@@ -26,6 +31,21 @@ public class ServerSpreadConnector {
             // Group for all servers in system (includes Load Balancers)
             SpreadGroup systemGroup = new SpreadGroup();
             systemGroup.join(spreadConn, "System");
+        }
+    }
+
+    public static void multicast(/* message */){
+        SpreadMessage m = new SpreadMessage();
+        m.addGroup("Servers");
+        // m.setData(message.toByteArray());
+        m.setSafe();
+        try {
+            if(spreadConn == null)
+                initializeConnector();
+
+            spreadConn.multicast(m);
+        } catch (SpreadException | UnknownHostException e) {
+            e.printStackTrace();
         }
     }
 }
