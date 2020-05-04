8476a2ab9c6b4dd2c9c13ef72879940a810a004a
joaop21 <jpsilva9898@gmail.com>
2020-05-04 09:21:41 +0100

    Add an change methods in Facade

--- src/main/java/Application/OnlineSuperMarket.java
+++ src/main/java/Application/OnlineSuperMarket.java
@@ -6,9 +6,10 @@ public interface OnlineSuperMarket {
     List<Item> getItems();
     Item getItem(int itemId);
     Item getItem(String itemName);
-    void addItemToCart(String username, int itemId);
+    boolean addItemToCart(String username, int itemId);
     void removeItemFromCart(String username, int itemId);
     List<Item> getCart(String username);
     boolean createOrder(String username);
     List<Order> getOrders(String username);
+    boolean login(String username, String password);
 }
