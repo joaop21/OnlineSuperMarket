db1a80145edcdb38a1830e06b9e72d1376e011bf
joaop21 <jpsilva9898@gmail.com>
2020-05-04 12:21:57 +0100

    Add gateway

---
+++ src/main/java/Middleware/Server/Gateway.java
@@ -0,0 +1,34 @@
+package Middleware.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Gateway implements Runnable {
+    private final int port;
+
+    public Gateway(int port){
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        ServerSocket ss = null;
+        try {
+            ss = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Socket s;
+        while (true) {
+            try {
+                assert ss != null;
+                s = ss.accept();
+                System.out.println("[SERVER - "+this.port+"]: Received new Connection");
+                // new Thread(new Skeleton(this.bank,spconn,s)).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
